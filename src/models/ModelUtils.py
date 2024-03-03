f91eea9e9e7cd189fe2a36abd675debf48989c13
dcuoliveira <dan.cunhaa@gmail.com>
2024-03-03 19:13:57 -0300

    refactor utils

---
+++ src/models/ModelUtils.py
@@ -0,0 +1,18 @@
+from models.ConvictionAdjustedMVO import ConvictionAdjustedMVO
+from models.Naive import Naive
+
+class ModelUtils:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def parse_portfolio_method(portfolio_method: str):
+        if portfolio_method == "naive":
+            return Naive
+        elif portfolio_method == "conviction_mvo":
+            return ConvictionAdjustedMVO
+        else:
+            raise ValueError(f"Invalid portfolio method: {portfolio_method}")
+    
+    def parse_regimes(self):
+        pass
