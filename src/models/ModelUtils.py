82f56d7e68dafeea6e7ecf5b64c6c4a1c3d1023c
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-08 20:46:31 -0300

    add weighted naive

--- src/models/ModelUtils.py
+++ src/models/ModelUtils.py
@@ -1,5 +1,6 @@
 from models.ConvictionAdjustedMVO import ConvictionAdjustedMVO
 from models.Naive import Naive
+from models.WeightedNaive import WeightedNaive
 
 class ModelUtils:
     def __init__(self):
@@ -9,6 +10,8 @@ class ModelUtils:
     def parse_portfolio_method(portfolio_method: str):
         if portfolio_method == "naive":
             return Naive
+        elif portfolio_method == "weighted-naive":
+            return WeightedNaive
         elif portfolio_method == "conviction_mvo":
             return ConvictionAdjustedMVO
         else:
