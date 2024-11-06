20adb28f4f4e5a1a80d1afdbe977ebbd9d9bc117
dcuoliveira <dan.cunhaa@gmail.com>
2024-11-06 18:25:48 +0000

    refactor naive code

--- src/models/Naive.py
+++ src/models/Naive.py
@@ -11,14 +11,12 @@ class Naive(PositionSizing):
         self.num_assets_to_select = num_assets_to_select
         self.strategy_type = strategy_type
 
-    def forward(self,
-                returns: pd.DataFrame,
-                regimes: pd.DataFrame,
-                current_regime: int,
-                transition_prob: np.ndarray,
-                regime_prob: np.ndarray,
-                **kwargs):
-
+    def compute_forecasts(self,
+                          returns: pd.DataFrame,
+                          regimes: pd.DataFrame,
+                          regime_prob: np.ndarray,
+                          transition_prob: np.ndarray,
+                          risk_adjusted: bool=True):
         TEMPERATURE = 0.85
         regime_prob_exp = np.exp(((regime_prob - regime_prob.mean()) / regime_prob.std()) / TEMPERATURE)
         next_regime_dist = np.matmul(regime_prob_exp / regime_prob_exp.sum(), transition_prob)[0]
@@ -32,7 +30,22 @@ class Naive(PositionSizing):
         next_regime_returns = labelled_returns[labelled_returns[cluster_name] == next_regime].drop(cluster_name, axis=1)
 
         # compute expected sharpe ratio on the next regime
-        forecasts = next_regime_returns.mean() / next_regime_returns.std()
+        forecasts = next_regime_returns.mean()
+
+        return (forecasts / next_regime_returns.std()) if risk_adjusted else forecasts, next_regime
+
+    def forward(self,
+                returns: pd.DataFrame,
+                regimes: pd.DataFrame,
+                transition_prob: np.ndarray,
+                regime_prob: np.ndarray,
+                **kwargs):
+
+        # compute expected sharpe ratio on the next regime
+        forecasts, next_regime = self.compute_forecasts(returns=returns,
+                                                        regimes=regimes,
+                                                        regime_prob=regime_prob,
+                                                        transition_prob=transition_prob)
 
         # generate positions
         positions = self.positions_from_forecasts(forecasts=forecasts,
