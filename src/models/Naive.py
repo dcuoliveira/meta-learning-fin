cf329cf700e0ab224012c67f1d7796debb132964
Dylan Sandfelder <dylansandfelder@gmail.com>
2024-11-19 12:51:00 +0000

    fix up naive

--- src/models/Naive.py
+++ src/models/Naive.py
@@ -17,12 +17,15 @@ class Naive(PositionSizing):
                 current_regime: int,
                 transition_prob: np.ndarray,
                 regime_prob: np.ndarray,
+                random_regime: bool = False,
                 **kwargs):
 
         TEMPERATURE = 0.85
         regime_prob_exp = np.exp(((regime_prob - regime_prob.mean()) / regime_prob.std()) / TEMPERATURE)
         next_regime_dist = np.matmul(regime_prob_exp / regime_prob_exp.sum(), transition_prob)[0]
         next_regime = np.argmax(next_regime_dist)
+        if random_regime:
+            next_regime = np.random.choice(list(range(6)))
 
         labelled_returns = pd.merge(returns, regimes, left_index=True, right_index=True)
 
